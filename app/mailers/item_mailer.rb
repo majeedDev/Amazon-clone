80a9b57fffdcc05fdd54db190966495230f3a7f9
Abdulmajeed Alasmari <ialasmariabdulmajeed@gmail.com>
2022-02-14 16:56:16 +0300

    adding some explaination

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -2,6 +2,7 @@ class ItemMailer < ApplicationMailer
 
     def new_item_mail(users)
         @item = params[:item]
+        #Adding some explainations
         mail(to: users.pluck(:email), subject: "New Item Added!")
     end
 
